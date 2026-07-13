728473b4a789b3a0bd50e0d7844e1b8116aaa60d
Florian Fellner <ffellner75@gmail.com>
2026-07-13 21:01:16 +0200

    ball shader

--- gdsg-semester-project-w-2025-/scripts/ball.gd
+++ gdsg-semester-project-w-2025-/scripts/ball.gd
@@ -6,16 +6,24 @@ extends CharacterBody2D
 @export var VELOCITY_THRESHOLD: float = 50.0
 @export var PADDLE_DIRECTION_INFLUENCE: float = 0.45
 @export var PADDLE_SPEED_INFLUENCE: float = 0.0
-@export var MIN_SCALE: float = 0.75
+@export var MIN_SCALE: float = 0.5
 @export var MAX_SCALE: float = 2
+@export var ROLL_SPEED: float = 0.01
+
+@export var SQUISH_AMOUNT: float = 0.8
+@export var STRETCH_AMOUNT: float = 1.2
+@export var SQUISH_TIME: float = 0.08
 
 var current_scale: float = 1.0
 var current_speed: float
 var last_hit_by: CharacterBody2D = player_1
 var is_split_spawn: bool = false
 var size_multiplier: float = 1.0
+var tile_offset := Vector2.ZERO
 
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: Sprite2D = $Visuals/Ball
+@onready var sprite_wrapper: Node2D = $Visuals
+@onready var sprite_material: ShaderMaterial = $Visuals/Ball.material
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var border_sound: AudioStreamPlayer = $"../border sound"
 @onready var fish_paddle_sound: AudioStreamPlayer = $"../fish paddle sound"
@@ -26,7 +34,6 @@ var size_multiplier: float = 1.0
 
 func _ready():
 	current_speed = 150.0
-	collision_shape.shape = collision_shape.shape.duplicate()
 	add_to_group("balls")
 	last_hit_by = player_1
 	print("Balls ", last_hit_by)
@@ -36,8 +43,9 @@ func _ready():
 		launch_ball()
 
 func _process(delta: float) -> void:
-	var multiplier: float = -1 if velocity.x < 0 else 1;
-	sprite.rotate(multiplier * delta * current_speed * 0.02);
+	tile_offset -= velocity * delta * ROLL_SPEED
+	sprite_material.set_shader_parameter("tile_offset", tile_offset);
+	sprite_material.set_shader_parameter("velocity", velocity);
 
 func launch_ball():
 	last_hit_by = player_1
@@ -82,7 +90,7 @@ func check_velocity_threshold() -> bool:
 func paw_hit(new_direction: Vector2, boost_amount: float):
 	velocity = new_direction.normalized()
 	current_speed += boost_amount
-	
+
 func scale_ball(multiplier: float):
 	var new_scale = clampf(current_scale * multiplier, MIN_SCALE, MAX_SCALE)
 	var actual_multiplier = new_scale / current_scale
