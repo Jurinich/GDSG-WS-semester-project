6c3b21bcb24f23c992a7861d19f6db661a20cbdf
Florian Fellner <ffellner75@gmail.com>
2026-07-20 20:46:37 +0200

    new paddle skin

--- gdsg-semester-project-w-2025-/scripts/ui/player_selection_node.gd
+++ gdsg-semester-project-w-2025-/scripts/ui/player_selection_node.gd
@@ -1,8 +1,10 @@
 class_name PlayerSelectionNode
 extends Control
 
-@onready var label: Label = $"Label"
-@onready var selection: TextureRect = $"Selection"
+@onready var label: Label = $Label
+@onready var selection: TextureRect = $Selection
+@onready var fish_name: Label = $MarginContainer/HBoxContainer/HBoxContainer/Fish
+@onready var emoji: Label = $MarginContainer/HBoxContainer/HBoxContainer/Emoji
 
 @export var player_name: String
 @export var paddles: Array[Paddle]
@@ -29,6 +31,8 @@ func get_selection() -> Paddle:
 
 func update_texture():
 	selection.texture = paddles[index].sprite
+	fish_name.text = paddles[index].name
+	emoji.visible = paddles[index].name == "fih"
 	if isP1:
 		selection.flip_h = true;
 
